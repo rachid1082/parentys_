f6c035f74f7da80ce3a539a627fba468c112153a
rachid1082 <boudaourachid1@gmail.com>
2026-03-08 11:50:37 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ❌ REMOVE this if you have it:
+  // output: "export",
+
+  // keep this if you want to ignore TS build errors
   typescript: {
     ignoreBuildErrors: true,
   },
